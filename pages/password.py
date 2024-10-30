2697c77ad68877bf13499d2d343fff48b72cdf2c
Charles M <charles.mittempergher@gmail.com>
2024-10-30 14:52:00 +0100

    nouvelle app Streamlit password

--- pages/password.py
+++ pages/password.py
@@ -11,10 +11,19 @@ saisie = st.text_input("Saisir votre mot de passe", type="password")
 if(st.button("Vérifier") or saisie):
     with st.spinner('Vérification'):
         time.sleep(3)
+    
     if(saisie!=pw):
         st.error("Mot de passe incorrect")
         st.image("denied.jpg", width=300)  
     else:
+        progress_text = "Operation in progress. Please wait."
+        my_bar = st.progress(0, text=progress_text)
+
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(1)
+        my_bar.empty()
         st.success("Mot de passe correct, bravo ")  
         st.snow()
         st.balloons()
