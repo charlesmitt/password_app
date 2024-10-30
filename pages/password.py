6f10d94dee6364ae2622378d0f7f2dc158f2736e
Charles M <charles.mittempergher@gmail.com>
2024-10-30 14:38:53 +0100

    nouvelle app Streamlit password

---
+++ pages/password.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import time
+pw = "123456"
+essais_restants = 3
+
+st.title("My new password app")
+st.write("SAISIR VOTRE SUPER MOT DE PASSE CI-DESSOUS")
+
+saisie = st.text_input("Saisir votre mot de passe", type="password")
+
+if(st.button("Vérifier") or saisie):
+    with st.spinner('Vérification'):
+        time.sleep(3)
+    if(saisie!=pw):
+        st.error("Mot de passe incorrect")
+        st.image("denied.jpg", width=300)  
+    else:
+        st.success("Mot de passe correct, bravo ")  
+        st.snow()
+        st.balloons()
+        st.image("image.png", width=300)  
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Bienvenue sur ma page")
+st.write("SAISIR VOTRE SUPER MOT DE PASSE DANS LA PAGE DE GAUCHE")
+ 
